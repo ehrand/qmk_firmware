a0036caac9af8943b627d5ea47fb8ffb473cc2ac
Andrei Ehrlich <andrei.ehrlich@gmail.com>
2022-05-30 07:41:58 +0200

    kyria: define SPLIT_USB_DETECT in keymaps/ehrand.
    Signed-off-by: Andrei Ehrlich <andrei.ehrlich@gmail.com>

--- keyboards/splitkb/kyria/keymaps/ehrand/config.h
+++ keyboards/splitkb/kyria/keymaps/ehrand/config.h
@@ -26,3 +26,11 @@
 
 // Lets you roll mod-tap keys
 #define IGNORE_MOD_TAP_INTERRUPT
+
+// ---------------------------------------------------------------------------------------
+
+// Modifications done by ehrand to customize kyria 2.1 (they may break other boards)
+
+// Some Pro Micro clones (especially the one used by me) fail to recognize
+// their couterparts. To fix this we need to define following macro.
+#define SPLIT_USB_DETECT
